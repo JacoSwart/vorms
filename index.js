f213531e5cbc9c31898c169019f55693abb3f678
Sas Swart <sas.swart.cdk@gmail.com>
2018-06-05 13:52:59 +0200

    Added Heroku PORT

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const app = express()
 
+const PORT = process.env.PORT || 5000 // Run on 80 locally, but use Heroku's port in production
+
 app.get('/hello', (req, res) => {
 	res.sendFile('User damage.htm');
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+app.listen(PORT, () => console.log('Example app listening on port 5000!'))
