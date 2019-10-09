db164409893da055393b6f1257d634cc4c77159d
Jaco Swart <jswart@telkomsa.net>
2019-10-09 19:59:18 +0200

    Multi DPI compatibility

--- static/khauleza.js
+++ static/khauleza.js
@@ -356,17 +356,17 @@
         
       };
 
-      window.onscroll = function() {myFunction()};
-        var navbar = document.querySelector("nav");
-        var sticky = navbar.offsetTop;
+      // window.onscroll = function() {myFunction()};
+      //   var navbar = document.querySelector("nav");
+      //   var sticky = navbar.offsetTop;
 
-        function myFunction() {
-          if (window.pageYOffset > sticky) {
-            navbar.classList.add("sticky")
-          } else {
-            navbar.classList.remove("sticky");
-          }
-        };
+      //   function myFunction() {
+      //     if (window.pageYOffset > sticky) {
+      //       navbar.classList.add("sticky")
+      //     } else {
+      //       navbar.classList.remove("sticky");
+      //     }
+      //   };
 
 
     });
