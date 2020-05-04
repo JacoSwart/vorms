e8c8776103d350a44f25d1d358a831c99837b711
Jaco <jswart@telkomsa.net>
2020-05-04 15:42:43 +0200

    Fix amount decimals

--- api/ddl/fuel_table.sql
+++ api/ddl/fuel_table.sql
@@ -1,8 +1,11 @@
-CREATE TABLE `vorms`.`fuel` (
+CREATE TABLE IF NOT EXISTS `vorms`.`fuel` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `liters` FLOAT UNSIGNED NOT NULL,
-  `amount` DECIMAL(10,2) UNSIGNED NOT NULL,
+  `amount` DECIMAL UNSIGNED NOT NULL,
   `vehicle` VARCHAR(8) NOT NULL,
   `date` DATE NOT NULL,
   `jobcard` VARCHAR(8) NOT NULL,
   PRIMARY KEY (`id`));
+
+ALTER TABLE `vorms`.`fuel` 
+  CHANGE COLUMN `amount` `amount` DECIMAL(10,2) UNSIGNED NOT NULL ;
